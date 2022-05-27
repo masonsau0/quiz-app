9ffaaac504407c4528c8ace1365939274ec8cc15
masonsau0 <masonsau0@gmail.com>
2022-05-27 18:29:56 -0400

    Add array of objects pertaining questions

---
+++ script.js
@@ -0,0 +1,44 @@
+// Array of objects containing the question, possible answers, and the correct answer for each question
+
+const quizData = [
+    {
+        question: 'What is 5 + 5?',
+        a: '10',
+        b: '1',
+        c: '5',
+        d: '25',
+        correct: 'a'
+    },
+    {
+        question: 'What is 5  x 3?',
+        a: '15',
+        b: '8',
+        c: '15',
+        d: '20',
+        correct: 'c'
+    },
+    {
+        question: 'What is 17 x 4?',
+        a: '67',
+        b: '78',
+        c: '53',
+        d: '68',
+        correct: 'd'
+    },
+    {
+        question: 'What is 22 + 105?',
+        a: '126',
+        b: '128',
+        c: '122',
+        d: '127',
+        correct: 'd'
+    },
+    {
+        question: 'What is 88 x 3?',
+        a: '188',
+        b: '264',
+        c: '244',
+        d: '256',
+        correct: 'b'
+    },
+]
