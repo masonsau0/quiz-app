505dbe9bc3f42996194d2086fd8ca09286c8740c
masonsau0 <masonsau0@gmail.com>
2022-05-27 22:47:18 -0400

    Finish rest of code

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const quizData = [
     },
     {
         question: 'What is 5  x 3?',
-        a: '15',
+        a: '10',
         b: '8',
         c: '15',
         d: '20',
@@ -43,6 +43,8 @@ const quizData = [
     },
 ];
 
+const answerEls = document.querySelectorAll('.answer');     // returns a nodelist of elements that match the specified selector name
+const quiz = document.getElementById('quiz');
 const questionEl = document.getElementById ('question');
 const  a_text = document.getElementById('a_text');
 const  b_text = document.getElementById('b_text');
@@ -51,10 +53,12 @@ const  d_text = document.getElementById('d_text');
 const submitButton = document.getElementById('submit');
 
 let currentQuestion = 0;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz() {
+    unselectAnswer();
     const currentQuizData = quizData[currentQuestion]     // assigns currentQuizData with the 0 index element of the quizData array
     questionEl.innerText = currentQuizData.question;     // gets the value of 'question:' in the 0 index element and displays it
     a_text.innerText = currentQuizData.a;     // gets the value of 'a:' in the 0 index element and displays it
@@ -63,7 +67,42 @@ function loadQuiz() {
     d_text.innerText = currentQuizData.d;
 }
 
+function getSelected() {
+
+    let answer = undefined;
+
+    answerEls.forEach( (answerEl) => {     // goes through each element in the nodelist
+        if(answerEl.checked) {
+            answer = answerEl.id     // gets the id of the choice selected
+        }
+    });
+    return answer;     // returns the selected choice to getSelected()
+}
+
+function unselectAnswer() {
+    answerEls.forEach( (answerEl) => {     // goes through each element in the nodelist
+        answerEl.checked = false;     // unselects the selected answer from the previous question when moving onto next question
+    });
+}
+
+
 submitButton.addEventListener('click', () => {     // function runs when submit button is clicked
-    currentQuestion++;     // increases the value of currentQuestion to move onto the next index/question
-    loadQuiz();     // moves on to next question
+    
+    const answer = getSelected();
+    if(answer) {     // only lets you move onto next question if a choice is selected
+        if(answer === quizData[currentQuestion].correct) {     // if the selected answer is correct increase the score
+            score++;
+        }
+        currentQuestion++;     // increases the value of currentQuestion to move onto the next index/question
+        if(currentQuestion < quizData.length) {
+            loadQuiz();     // moves on to next question
+        }  else {
+            quiz.style.flex= '0 1 auto';
+            quiz.style.textAlign = 'center';
+            quiz.style.justifyContent = 'center';
+            quiz.innerHTML = `<h2>You scored ${score} out of ${quizData.length}!</h2>     
+             <button onclick="location.reload()" id="replay-button">Try Again</button>`;     // displays end score message
+             // adds replay button | location.reload() refreshes webpae upon click
+        }
+    }
 });
